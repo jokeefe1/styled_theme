3e2886d91fa7343a9f7da424a9c3199732e38eac
jokeefe1 <jokeefe@weegoglobal.com>
2018-08-23 16:28:26 -0400

    Added Routing and Navbar
    created routing and navigation skeleton for a Single Page App

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import Wrapper from "../styled/Wrapper";
+import Nav from "../styled/Nav";
 
 const Navbar = props => (
   <Wrapper>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Home
-    </NavLink>
-    <NavLink to="/portfolio" activeClassName="is-active">
-      Portfolio
-    </NavLink>
-    <NavLink to="/contact" activeClassName="is-active">
-      Contact
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
+    <Nav>
+      <NavLink to="/" activeClassName="is-active" exact={true}>
+        Home
+      </NavLink>
+      <NavLink to="/portfolio" activeClassName="is-active">
+        Portfolio
+      </NavLink>
+      <NavLink to="/contact" activeClassName="is-active">
+        Contact
+      </NavLink>
+      <NavLink to="/help" activeClassName="is-active">
+        Help
+      </NavLink>
+    </Nav>
   </Wrapper>
 );
 
